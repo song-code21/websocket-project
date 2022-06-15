341c81b84eed0b32a01da3325b4d65bf5c42b228
hakyoung <hakyoung.song@softwareinlife.com>
2022-06-15 19:18:32 +0900

    server: 소켓 서버 추가 및 메시지 전송 기능 추가

--- src/server.js
+++ src/server.js
@@ -1,7 +1,29 @@
 import express from 'express';
+import {WebSocket} from "ws";
+import http from "http";
 
 const app = express();
 
-console.log('hello');
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render("home"));
 
-app.listen(8080)
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("connected to browser")
+    socket.on("close", () => {
+        console.log("disconnected from the browser")
+    });
+    socket.on("message", message => sockets.forEach((aSocket) => aSocket.send(message)))
+})
+//connection이 생기면 socket을 받는다.
+//socket의 메소드
+
+server.listen(3000, handleListen)
